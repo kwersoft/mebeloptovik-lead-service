07830a31040cc04fa52a79530c8cc0ec29004e69
Сергей Брагин <bsm@kcentr.ru>
2020-08-11 14:00:48 +0400

    The first commit

---
+++ Dockerfile
@@ -0,0 +1,28 @@
+FROM php:7
+
+RUN set -eux ; \
+    apt-get update -yqq &&  \
+    apt-get install -yqq    \
+    libmcrypt-dev           \
+    libpq-dev               \
+    librabbitmq-dev         \
+    git                     \
+    libzip-dev              \
+    unzip                   \
+    wget					\
+	cron 					\
+    rsyslog
+
+RUN set -eux ; docker-php-ext-install bcmath sockets zip
+
+RUN set -eux ; pecl install amqp
+
+RUN set -eux ; docker-php-ext-enable amqp
+
+COPY --from=composer:latest /usr/bin/composer /usr/bin/composer
+COPY cron /etc/cron.d/sample
+COPY composer.json /app/composer.json
+
+WORKDIR /app
+
+CMD composer install && service rsyslog start && service cron start && touch /var/log/cron.log && tail -f /var/log/cron.log
---
+++ deamon.php
@@ -0,0 +1,82 @@
+<?php
+require_once __DIR__ . '/vendor/autoload.php';
+
+//Необходимые классы
+use PhpAmqpLib\Connection\AMQPStreamConnection;
+use PhpAmqpLib\Message\AMQPMessage;
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+$MEBEL_OPTOVIK_ID = $_ENV['MEBEL_OPTOVIK_ID'] ?? '';
+$RABBIT_HOST = $_ENV['RABBIT_HOST'] ?? '';
+$RABBIT_PORT = $_ENV['RABBIT_PORT'] ?? '';
+$RABBIT_LOGIN = $_ENV['RABBIT_LOGIN'] ?? '';
+$RABBIT_PSWD = $_ENV['RABBIT_PSWD'] ?? '';
+$RABBIT_CHANNEL_NAME = $_ENV['RABBIT_CHANNEL_NAME'] ?? '';
+$RABBIT_V_HOST = $_ENV['RABBIT_V_HOST'] ?? '';
+
+/**
+ * пример работы с гугл таблицами https://codd-wd.ru/primery-google-sheets-tablicy-api-php/
+ */
+
+//Подключаемся к сервису гугл таблиц
+$googleAccountKeyFilePath = __DIR__ . '/bender-oauth.json';
+putenv('GOOGLE_APPLICATION_CREDENTIALS=' . $googleAccountKeyFilePath);
+$client = new Google_Client();
+$client->useApplicationDefaultCredentials();
+$client->addScope('https://www.googleapis.com/auth/spreadsheets');
+$service = new Google_Service_Sheets($client);
+
+//получение заголовков таблицы
+$data = $service->spreadsheets_values->get($MEBEL_OPTOVIK_ID, "Заявки!B1:P1");
+if (empty($data->values)) exit;
+$headers = $data->values[0];
+
+//получение строки значений
+$data = $service->spreadsheets_values->get($MEBEL_OPTOVIK_ID, "Заявки!B2:P2");
+if (empty($data->values)) exit;
+$values = $data->values[0];
+
+//формирование сообщения
+$message = ['type' => 'google_sheets', 'data' => []];
+$data = ['site' => 'МебельОптовик'];
+foreach ($headers as $key => $head) {
+	$data[$head] = $values[$key];
+}
+$message['data'] = $data;
+
+try {
+	//Создание подключение к очереди
+	$connection = new AMQPStreamConnection($RABBIT_HOST, $RABBIT_PORT, $RABBIT_LOGIN, $RABBIT_PSWD, $RABBIT_V_HOST);
+	$channel = $connection->channel();
+	$channel->queue_declare($RABBIT_CHANNEL_NAME, false, true, false, false);
+	
+	//создаем сообщение в очередь
+	$msg = new AMQPMessage(json_encode($message, JSON_UNESCAPED_UNICODE));
+	$channel->basic_publish($msg, '', $RABBIT_CHANNEL_NAME);
+	$channel->close();
+	$connection->close();
+} catch (\Throwable $th) {
+	echo (json_encode($th->getMessage(), JSON_UNESCAPED_UNICODE));
+	exit;
+}
+echo $message;
+//TODO Удаление обработанной строки
+$requests = [
+	new Google_Service_Sheets_Request([
+		'deleteRange' => [
+			'range' => [
+				'sheetId' => 660544691,
+				'startRowIndex' => 1,
+				'endRowIndex' => 2,
+			],
+			'shiftDimension' => 'ROWS'
+		]
+	])
+];
+$batchUpdateRequest = new Google_Service_Sheets_BatchUpdateSpreadsheetRequest([
+	'requests' => $requests
+]);
+$service->spreadsheets->batchUpdate($MEBEL_OPTOVIK_ID, $batchUpdateRequest);
+exit;
